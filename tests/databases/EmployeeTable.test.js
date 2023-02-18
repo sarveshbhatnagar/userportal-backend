9bb9168854e829bbc00d9fd0f7b0acbc3d397463
Sarvesh Bhatnagar <sarveshbhatnagar08@gmail.com>
2023-02-17 20:56:07 -0500

    fixing beforeEach and afterEach

--- tests/databases/EmployeeTable.test.js
+++ tests/databases/EmployeeTable.test.js
@@ -1,28 +1,22 @@
 import {EmployeeTable} from '../../repository/tables/employeeTable';
 import sinon from 'sinon';
 import {expect} from 'chai';
-import { AWSProvider } from '../../repository/providers/awsProvider';
-import { afterEach, beforeEach } from 'node:test';
 import { DATABASE } from '../../utils/constants';
-
-// const fakeDatabase = sinon.stub(AWSProvider, 'get')
-
-// fakeDatabase.withArgs({Key: {id: '123'}, tableName: 'st-employee'}).returns({
-//     Item: {id: '123', name: 'John Doe'}
-// });
-
-// fakeDatabase.withArgs({Key: {id: '234'}, tableName: 'st-employee'}).returns({});
-
-// const dynamoDbMock = {
-//     get: fakeDatabase
-// };
 
 
 describe('EmployeeTable', () => {
     let getStub;
+    
+    beforeEach(() => {
+        getStub = sinon.stub(DATABASE, 'get');
+    });
+
+    afterEach(() => {
+        getStub.restore();
+    });
 
     it('should get an employee', async () => {
-        getStub = sinon.stub(DATABASE, 'get');
+        
         const expectedParams = {
             Key: {id: '123'},
             TableName: 'st-employee'
@@ -35,11 +29,9 @@ describe('EmployeeTable', () => {
         const employee = await EmployeeTable.getEmployee('123');
 
         expect(employee).to.eql({id: '123', name: 'John Doe'});
-        getStub.restore();
     });
 
     it('should return error response on miss for an employee', async () => {
-        getStub = sinon.stub(DATABASE, 'get');
         const expectedParams = {
             Key: {id: '234'},
             TableName: 'st-employee'
@@ -50,6 +42,5 @@ describe('EmployeeTable', () => {
         const employee = await EmployeeTable.getEmployee('234');
 
         expect(employee).to.eql(undefined);
-        getStub.restore();
     });
 });

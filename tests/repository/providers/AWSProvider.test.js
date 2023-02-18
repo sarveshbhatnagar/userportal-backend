6ffce0d41b3b17d39973039e9c5d1b26260263d4
Sarvesh Bhatnagar <sarveshbhatnagar08@gmail.com>
2023-02-17 21:37:16 -0500

    adding tests for get

--- tests/repository/providers/AWSProvider.test.js
+++ tests/repository/providers/AWSProvider.test.js
@@ -27,6 +27,71 @@ describe('AWSProvider', () => {
             expect(instance).to.equal(client);
         });
     });
-      
 
+
+    describe('get', () => {
+        let getStub;
+
+        beforeEach(() => {
+            AWSProvider.dynamoDb = new AWS.DynamoDB.DocumentClient();
+            getStub = sinon.stub(AWSProvider.dynamoDb, 'get');
+            
+        });
+
+        afterEach(() => {
+            getStub.restore();
+        });
+
+        it('should get an item for get operation! e.g. {id:21, name:sksk}', async () => {
+            const params = {
+                Key: {id: '123'},
+                TableName: 'st-employee'
+            };
+
+            const expectedResponse = {
+                Item: {id: '123', name: 'John Doe'}
+            };
+
+            getStub.withArgs(params)
+            .returns({
+                promise: () => Promise.resolve(expectedResponse)
+            });
+
+            const item = await AWSProvider.get(params)
+            .then((data) => data)
+            .catch((err) => {
+                console.log("Error getting data: ", err);
+                throw err;
+            });
+            
+            // It Returns the Item i.e. {id: '123', name: 'John Doe'}
+            expect(item).to.eql(expectedResponse.Item);
+        });
+
+        it('should return error response on server rejection', async () => {
+            const params = {
+                Key: {id: '123'},
+                TableName: 'st-employee'
+            };
+
+            getStub.withArgs(params)
+            .returns({
+                promise: () => Promise.reject(new Error('Server Error'))
+            });
+
+            const item = AWSProvider.get(params);
+            let er;
+            try {
+                await item;
+            } catch (error) {
+                er = error;
+                
+            }
+
+            expect(er).to.be.an.instanceOf(Error);
+            expect(er.message).to.equal('Server Error');
+
+    });
+      
+    });
 });

1f6d69fd4c05c2ed4dd6a96caf5981bdaf917405
Sarvesh Bhatnagar <sarveshbhatnagar08@gmail.com>
2023-02-22 21:36:13 -0500

    refactoring code

--- services/authentication.js
+++ services/authentication.js
@@ -2,31 +2,21 @@
 import { buildResponse } from '../utils/utils';
 import { validateLogin } from '../validators/loginValidator';
 class Authentication {
-   
-
 
     login(user){
         const validationResult = validateLogin(user);
-
         if(validationResult.error){
             return validationResult.getErrorResponse();
         }
-        // TODO get user from database
-        // TODO compare password
-        // TODO return token with response.
         return buildResponse(200, {message: "Login successful"});
     }
 
     register(user){
         const validationResult = validateRegister(user);
-
         if(validationResult.error){
             return validationResult.getErrorResponse();
         }
         return buildResponse(200, {message: "Register Successfull"})
     }
-
-
-
 
 }
